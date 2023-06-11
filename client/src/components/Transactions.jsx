97353df936349bec893c2201111a3d2163e245d0
josephbanaag <josephrbanaag@gmail.com>
2023-06-12 00:02:09 +0800

    completing the designs and added transactions

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -3,6 +3,77 @@ import { TransactionContext } from "../context/TransactionContext";
 
 import dummyData from "../utils/dummyData";
 
+import { shortenAddress } from "../utils/shortenAddress";
+import useFetch from "../hooks/useFetch";
+
+const TransactionCard = ({
+  addressTo,
+  addressFrom,
+  timestamp,
+  message,
+  keyword,
+  amount,
+  url,
+}) => {
+  const gifUrl = useFetch({ keyword });
+
+  return (
+    <div
+      className="bg-[#181918] m-4 flex flex-1
+  2xl:min-w-[240px] 
+  2xl:max-w-[500px]
+  sm:min-w-[270px] 
+  sm:max-w-[300px]
+  flex-col
+  p-3
+  hover:shadow-2xl
+  "
+    >
+      <div className="flex flex-col items-center w-full mt-3">
+        <div className="w-full mb-6 p-2">
+          <a
+            href={`https://sepolia.etherscan.io/address/${addressFrom}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="text-white text-base">
+              From: ${shortenAddress(addressFrom)}
+            </p>
+          </a>
+
+          <a
+            href={`https://sepolia.etherscan.io/address/${addressTo}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="text-white text-base">
+              To: ${shortenAddress(addressTo)}
+            </p>
+          </a>
+
+          <p className="text-white text-base">Amount: {amount} ETH</p>
+
+          {message && (
+            <>
+              <br />
+              <p className="text-white text-base">Message:{message}</p>
+            </>
+          )}
+        </div>
+        <img
+          src={gifUrl || url}
+          alt="nature"
+          className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
+        />
+
+        <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+          <p className="bg-[#37c7da] font-bold">{timestamp}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Transactions = () => {
   const { currentAccount } = useContext(TransactionContext);
   return (
@@ -18,6 +89,12 @@ const Transactions = () => {
               Connect your account to see the latest transaction
             </h3>
           )}
+
+          <div className="flex flex-wrap justify-center items-center mt-10">
+            {dummyData.reverse().map((transaction, i) => (
+              <TransactionCard key={i} {...transaction} />
+            ))}
+          </div>
         </div>
       </div>
     </>
